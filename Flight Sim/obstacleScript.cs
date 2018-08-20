06c863d9ad12e64c1d2dc023084866fd34047787
Steve C <scasica0@csu.fullerton.edu>
2018-08-20 10:56:47 -0700

    Add files via upload

---
+++ Flight Sim/obstacleScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacleScript : MonoBehaviour {
+
+	int pos = 0;
+	bool reverse = false;
+	public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (pos > 100){
+			reverse = true;
+		}
+		else if (pos < -100){
+			reverse = false;
+		}
+		if (reverse) {
+			pos -= 1;
+		} 
+		else {
+			pos += 1;
+		}
+
+		this.transform.Translate (new Vector3 (pos, 0,0 ) * Time.deltaTime * speed);
+	}
+}
---
+++ Flight Sim/planeScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class planeScript : MonoBehaviour {
+
+	public float speed = 30.0f;
+	public GameObject explosionEffect;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.forward * Time.deltaTime * speed;
+
+		transform.Rotate(Input.GetAxis("Vertical"),0.0f,Input.GetAxis("Horizontal"));
+
+	}
+		
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("crashing")) {
+			explosionEffect.SetActive (true);
+			player.SetActive (false);
+			speed = 0.0f;
+		}
+	}
+
+}
