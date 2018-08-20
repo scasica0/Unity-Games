7cb6d5be2d2cbe4b11087445754d1af70380222d
Steve C <scasica0@csu.fullerton.edu>
2018-08-20 11:05:08 -0700

    Add files via upload

---
+++ FPS/enemyScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScript : MonoBehaviour {
+
+	bool isShot = false;
+
+	public void MakeDamage(){
+		if (!isShot) {
+			GetComponent<Animation> ().Play ("back_fall");
+			isShot = true;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isShot) {
+			GetComponent<Animation> ().Play ("walk");
+			this.transform.Translate (new Vector3 (0, 0, 1) * Time.deltaTime);
+			this.transform.Rotate (new Vector3 (0, Random.Range (-90, 90), 0) * Time.deltaTime);
+		}
+	}
+}
---
+++ FPS/gunScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			GetComponent<AudioSource> ().Play ();
+			GetComponent<Animation> ().Play ();
+
+			RaycastHit hit;
+			Physics.Raycast (
+				this.transform.position,
+				this.transform.TransformDirection (Vector3.forward),
+				out hit);
+			hit.transform.SendMessage ("MakeDamage");
+		}
+	}
+}
